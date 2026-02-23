bac3177d6ef3b074e5df2f3767574b9b63a0ba37
Senosiain, Miguel <miguel.senosiain@mmc.com>
2026-02-23 19:34:26 -0300

    Fix issue members list scrool and search

--- apps/ui/src/environments/environment.production.ts
+++ apps/ui/src/environments/environment.production.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   apiBaseUrl: 'https://parking-control-be.lostordos.com.ar/api/v1',
-  parkingSpaces: 80,
+  parkingSpaces: 78,
   // Keys used to store tokens in localStorage
   accessTokenKey: 'access_token',
   refreshTokenKey: 'refresh_token',
